5848878134af2a87b4a80b4033cf188193c9cb33
Jes Coyle <jes.r.coyle@gmail.com>
2016-10-28 10:42:47 -0700

    working on lesson 1

---
+++ R-lessons/Code/R-lesson1.r
@@ -0,0 +1,16 @@
+## Name:
+
+
+### Getting Started
+
+# Comments begin with a # and give you information about what the code does.
+# For example, the next line is code that will print 'Hello' in the console.
+print('Hello')
+
+
+
+### Objects 
+
+## Vectors
+
+
