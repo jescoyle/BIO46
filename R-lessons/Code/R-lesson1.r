2f411aa8c2bf1ebce626fea772f2147d3f163619
Jes Coyle <jes.r.coyle@gmail.com>
2017-01-04 10:35:39 -0800

    removed matrix and arithmatic sections from lesson 1 to shorten it. Will cover arithmetic in lesson 2.

--- R-lessons/Code/R-lesson1.r
+++ R-lessons/Code/R-lesson1.r
@@ -35,8 +35,13 @@ fingers
 lichen = c('ascomycete','algae','yeast','bacteria')
 lichen
 
-# YOUR TURN: Make a vector with the first 4 even numbers named 'evens'.
+# Make a vector called animals
+animals = c('cat','parrot','fly','squid','seastar')
 
+# How many elements are in lichen?
+length(lichen)
+
+# YOUR TURN: Make a vector with the first 4 even numbers named 'evens'.
 
 
 # Repeat something multiple times
@@ -55,45 +60,26 @@ seq(-10, 3, 0.5)
 # YOUR TURN: Make a vector named 'trials' with the integers from 1 to 50 repeated 3 times.
 
 
-## Matrices and Arrays
-
-# Make a 3 x 4 matrix filled of 0s
-matrix(0, 3, 4)
-
-# Make a 3 x 3 matrix with the integers 1 - 9
-matrix(1:9, 3, 3)
-
-# Make a 3-D array filled with cats
-array('cat', dim = c(2,3,4))
-
-# Make a matrix with more animals
-animals = c('cat','parrot','fly','squid','seastar')
-my_zoo = matrix(animals, 3, 5)
-
-# YOUR TURN: Make a matrix named trial_matrix with three columns, 
-#            each of which contains the numbers 1 to 50 in increasing order.
-
-
-
 ## Lists and Dataframes
 
 # Make some lists
 list('My', 'dog', 'has', 10, 'fleas.')
 list(c('My','dog','has'), 10, 'fleas.')
-list(animals, my_zoo)
 lichen_list = list('lichen', lichen)
+
+# How many elements in lichen_list?
+length(lichen_list)
 
 # Make a dataframe
 data.frame(1:4, lichen)
 data.frame(part = 1:4, organism = lichen)
 
 # YOUR TURN:
-#   Add a vector containing the first four even numbers to the beginning of `lichen_list`.
+#   Make a list containing two elements: a vector of the first four even numbers and the vector lichen.
 #   Make a dataframe called 'hand' whose first column has the integers 1 to 5, 
 #       second column contains finger names (starting with 'thumb'), 
 #       and third column contains `animals`.
 #   Name the columns 'number','name','animal'.
-
 
 
 ## Accessing elements in objects
@@ -117,36 +103,35 @@ lichen[c(1,4)]
 # Display the elements of lichen in reverse the order
 lichen[4:1]
 
-# Which animal is in the 2nd row and 4th column of my_zoo?
-my_zoo[2,4]
-
-# Which animals are in the 3rd row of my_zoo?
-my_zoo[3,]
-
-# Which animals are in the 4th and 5th columns of my_zoo?
-my_zoo[,c(4,5)]
-
 # BONUS: Change the last two odd numbers in `lichen_list` back to even numbers.
 
-# Name the rows and columns of a matrix
-rownames(my_zoo) = c('A','B','C')
-colnames(my_zoo) = c('blue','pink','red','yellow','green')
-my_zoo
 
-# Get the animals in the blue column in rows A and B
-my_zoo[c('A','B'), 'blue']
+# What is in the 2nd row and 3rd column of hand?
+hand[2,3]
+
+# What is in the 3rd row of hand?
+hand[3,]
+
+# What is the 5th animal and finger name?
+hand[5, c(3,2)]
 
 # Create a data frame
 my_data = data.frame(1:20, lichen, rep(1:5, each=4))
 
+# View the dataframe column names given by default
+names(my_data)
+
 # Change the column names
 names(my_data) = c('trial', 'organism', 'treatment')
 names(my_data)
-colnames(my_data)
 my_data
+
+# Elements in a vector are not assigned names by default
+names(animals)
 
 # Name the elements in a vector
 names(animals) = c('Felix', 'Polly', 'Charlotte', 'Kraken', 'Patrick')
+animals
 
 # What type of animal is Patrick?
 animals['Patrick']
@@ -188,65 +173,31 @@ seq(10, 20, length.out=6)
 #            each with the numbers 1 to 5, in order.
 
 
-
-## Arithmetic
-
-# Add two numbers
-2+3
-
-# Add the first two elements of fingers
-fingers[1] + fingers[2]
-
-# Subtraction
-monkey = 6 - 2
-monkey
-
-# Division'
-monkey / 2
-
-# Multiplication
-monkey*3
-
-# Exponents
-3^2
-
-# Order of operations
-5+2*4^2
-(5+2)*4^2
-5+(2*4)^2
-
-# Create two vectors
-troupe = rep(monkey, 7)
-troupe
-bananas = seq(-3,3)
-bananas
-
-# Arithmetic with vectors and scalars
-bananas + 3
-bananas * 2
-bananas^2
-
-# Vector arithmetic
-troupe + bananas
-troupe - bananas
-troupe * bananas
-troupe / bananas
-troupe^bananas
-
-# YOUR TURN: Add a column to `my_data` called `temp_F` which is the Fahrenheit equivalent of 
-#            the Celcius temperature displayed in `temp_C`.
-
-
-
 ### Let's Review
 
 # YOUR TURN: For each of the following lines of code, write a comment above it describing (briefly) 
 #            what it will do. Then, run the line of code to check your answer. Modify your comment if necessary.
 
 
+fox = 2:8
 
 
+c(fox, 2)
 
+
+rep(fox, 3)
+
+
+seq(0, 10, 2)
+
+
+list('myfox', fox)
+
+
+fox_data = data.frame(fox, rep('hare', 7))
+
+
+names(fox_data)[2] = 'prey'
 
 
 
